4a2e267cac70265c70615dba38c47d97205a7b8d
genmiyazato <sapporogentaro@gmail.com>
2021-01-07 16:07:15 +0900

    init

---
+++ js/swiper.js
@@ -0,0 +1,21 @@
+/*HTML テンプレート
+<!-- スライダーのメインコンテナの div 要素 -->
+<div class="swiper-container"> 
+  <!-- ラッパーの（スライドを囲む） div 要素 -->
+  <div class="swiper-wrapper"> 
+    <!-- それぞれのスライドの div 要素 -->
+    <div class="swiper-slide"><img src="images/sample_01.png" alt=""></div>
+    <div class="swiper-slide"><img src="images/sample_02.png" alt=""></div>
+    <div class="swiper-slide"><img src="images/sample_03.png" alt=""></div>
+  </div>
+  <!-- ページネーションの div 要素 -->
+  <div class="swiper-pagination"></div>
+ 
+  <!-- ナビゲーションボタンの div 要素-->
+  <div class="swiper-button-prev"></div>
+  <div class="swiper-button-next"></div>
+ 
+  <!-- スクロールバーの div 要素 -->
+  <div class="swiper-scrollbar"></div>
+</div>
+*/
