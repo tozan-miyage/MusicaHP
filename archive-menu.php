ecf23a5d6411d46512c6195f2ffb9f9f64c55d4f
genmiyazato <sapporogentaro@gmail.com>
2021-01-10 07:38:38 +0900

    page-book.php fineshed

--- archive-menu.php
+++ archive-menu.php
@@ -11,24 +11,47 @@ menu.css
                 <h2>[<span>menu</span>]</h2>
                 <!-- <p>全てシャンプー・スタイリング込</p> -->
             </div>
+            <?php
+            $args = array(
+                'post_type' => array('cut','color','perm', 'straight_perm', 'head_spa'),
+                'order' =>  'ASC',
+            );
+            $menu_query = new WP_Query($args);
+            $post_index = 0;
+            ?>
+            <?php if ($menu_query->have_posts()) : ?>
+                <?php while ($menu_query->have_posts()) : ?>
+                    <?php $menu_query->the_post(); ?>
+                    <div class="concept menu-hedding">
+                        <h3><?php the_title(); ?><span><?php the_content(); ?></span></h3>
+                    </div>
+                    <div class="content-sentence">
+                        <?php
+                        $fields = get_fields();
+                        ?>
+                        <?php if ($fields) : ?>
+                            <?php foreach ($fields as $name => $value) : ?>
+                                <?php if ($value !== "") : ?>
+                                    <div class="menu-price-wrapper">
+                                        <div class="menu"><?php echo $name; ?></div>
+                                        <div class="price">¥<?php echo number_format($value); ?></div>
+                                    </div>
+                                <?php endif; ?>
+                            <?php endforeach; ?>
+                        <?php endif; ?>
 
-            <div class="concept menu-hedding">
-                <h3>カット</h3>
-            </div>
-            <div class="content-sentence">
-                <div class="menu-price-wrapper">
-                    <div class="menu">一般カット</div>
-                    <div class="price">¥4,080</div>
-                </div>
-                <div class="menu-price-wrapper">
-                    <div class="menu">男性カット</div>
-                    <div class="price">¥3,370</div>
-                </div>
-                <div class="menu-price-wrapper">
-                    <div class="menu">高校生カット</div>
-                    <div class="price">¥3,060</div>
-                </div>
-            </div>
+                        <!-- <div class="menu-price-wrapper">
+                            <div class="menu">男性カット</div>
+                            <div class="price">¥3,370</div>
+                        </div>
+                        <div class="menu-price-wrapper">
+                            <div class="menu">高校生カット</div>
+                            <div class="price">¥3,060</div>
+                        </div> -->
+                    </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
+            <?php wp_reset_postdata(); ?>
         </div>
     </div>
 </section>
---
+++ page-menu.php
@@ -0,0 +1,147 @@
+<?php get_header(); ?>
+
+<!-- =============================================
+menu.css
+============================================= -->
+
+<section id="menu">
+    <div class="row content">
+        <div class="col-12 content-wrapper">
+            <div class="content-headding">
+                <h2>[<span>menu</span>]</h2>
+            </div>
+        </div>
+    </div>
+    <!-- ---------カットメニュー--------- -->
+    <div id="cut" class="row content">
+        <div class="col-12 menus">
+            <div class="concept menu-hedding">
+                <h3>カット<br><span class="supplement">(シャンプー・スタイリング込)</span></h3>
+            </div>
+            <div class="content-sentence">
+
+                <div class="menu-price-wrapper">
+                    <div class="menu">一般カット</div>
+                    <div class="price">¥4,080</div>
+                </div>
+                <div class="menu-price-wrapper">
+                    <div class="menu">男性カット</div>
+                    <div class="price">¥3,370</div>
+                </div>
+                <div class="menu-price-wrapper">
+                    <div class="menu">高校生<span class="supplement">（男性）</span><br>中学生カット</div>
+                    <div class="price">¥2,550</div>
+                </div>
+                <div class="menu-price-wrapper">
+                    <div class="menu">小学生まで</div>
+                    <div class="price">¥2,160</div>
+                </div>
+                <div class="menu-price-wrapper front-mainte">
+                    <div class="menu">前髪<span class="supplement">（メンテナンス）</span></div>
+                    <div class="price">¥550</div>
+                </div>
+                <div class="menu-price-wrapper front-new">
+                    <div class="menu">&emsp;&emsp;<span class="supplement">（新規）</span></div>
+                    <div class="price">¥1,100</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- ---------カラーメニュー--------- -->
+    <div id="color" class="row content">
+        <div class="col-12 menus">
+            <div class="concept menu-hedding">
+                <h3>カラー<br><span class="supplement">(カット・シャンプー・スタイリング込)</span></h3>
+            </div>
+            <div class="content-sentence">
+                <div class="menu-price-wrapper">
+                    <div class="menu">シングル</div>
+                    <div class="price">¥4,080〜</div>
+                    <div class="supplement-box">
+                        <span class="supplement">ナチュラルな 1 工程のカラー</span>
+                    </div>
+                </div>
+                <div class="menu-price-wrapper">
+                    <div class="menu">香草</div>
+                    <div class="price">¥4,080〜</div>
+                    <div class="supplement-box">
+                        <span class="supplement">漢方・ハーブが主成分のカラー</span>
+                        <span class="supplement">頭皮が弱い方向けの白髪染め(髪の色を明るくする事は出来ません)</span>
+                        <span class="supplement">合成界面活性剤、防腐剤、シリコーン、ポリマー、香料、着色料不使用</span>
+                    </div>
+                </div>
+                <div class="menu-price-wrapper">
+                    <div class="menu">ヘアマニキュア</div>
+                    <div class="price">¥7,650〜</div>
+                </div>
+                <div class="menu-price-wrapper">
+                    <div class="menu">ダブルプロセス</div>
+                    <div class="price">¥13,260〜</div>
+                    <div class="supplement-box">
+                        <span class="supplement">ブリーチを使った2工程のカラー</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- ---------パーマーメニュー--------- -->
+    <div id="perm" class="row content">
+        <div class="col-12 menus">
+            <div class="concept menu-hedding">
+                <h3>パーマ<br><span class="supplement">(カット・シャンプー・スタイリング込)</span></h3>
+            </div>
+            <div class="content-sentence">
+                <div class="menu-price-wrapper">
+                    <div class="menu">パーマ</div>
+                    <div class="price">¥9,080〜</div>
+                </div>
+                <div class="menu-price-wrapper">
+                    <div class="menu">ポイントパーマ</div>
+                    <div class="price">¥6,100〜</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- ---------縮毛矯正・ストレートーメニュー--------- -->
+    <div id="straght-perm" class="row content">
+        <div class="col-12 menus">
+            <div class="concept menu-hedding">
+                <h3>縮毛矯正・ストレート<br><span class="supplement">(カット・シャンプー・スタイリング込)</span></h3>
+            </div>
+            <div class="content-sentence">
+                <div class="menu-price-wrapper">
+                    <div class="menu">縮毛矯正・ストレート</div>
+                    <div class="price">¥12,240〜</div>
+                    <div class="supplement-box">
+                        <span class="supplement">高校生まで10％OFF</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- ---------ヘッドスパメニュー--------- -->
+    <div id="head-spa" class="row content">
+        <div class="col-12 menus">
+            <div class="concept menu-hedding">
+                <h3>ヘッドスパ</h3>
+            </div>
+            <div class="content-sentence">
+                <div class="menu-price-wrapper">
+                    <div class="menu">ヘッドスパ</div>
+                    <div class="price">¥3,310〜</div>
+                    <div class="supplement-box">
+                        <span class="supplement">頭皮と毛髪の健康を促すマッサージ。頭皮クレンジングで毛穴に詰まった汚れを除去し、頭皮マッサージで血流をよくします。</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
